9bf03e0c7bec13515ce80c2a46a112d9c8647368
dilobar2807 <ahrorbekshuhratbekov@gmail.com>
2025-09-12 13:23:44 +0500

    Create string.py

---
+++ lesson-1/homework/string.py
@@ -0,0 +1,21 @@
+#Given a side of square. Find its perimeter and area.
+a=int(input("Kvadratning tomonini kiriting: "))
+print("Yuzasi: ","S=",a**2, ", Perimetri: ", "P=", 4*a)
+
+#Given diameter of circle. Find its length.
+a=int(input("Aylananing diametrini kiriting: "))
+print("Aylana uzunligi ", 3.14*a, " ga teng.")
+
+
+#Given two numbers a and b. Find their mean.
+
+a=int(input("1-sonni kiriting: "))
+b=int(input("2-sonni kiriting: "))
+print("Sonlarning o'rtacha qiymati: ", (a+b)/2)
+
+# Given two numbers a and b. Find their sum, product and square of each number.
+a=int(input("a sonni kiriting: "))
+b=int(input("b sonni kiriting: "))
+print("a+b=", a+b)
+print("a*b=", a*b)
+print("a ning kvadrati: ", a**2,  ", b ning kvadrati: ", b**2)
